222d833616dd93e1fed47c1a615125763c79938a
Arco <49058858+arcofb@users.noreply.github.com>
2020-09-03 19:25:05 +0700

    Update monero.py

--- monero.py
+++ monero.py
@@ -5,8 +5,6 @@ os.system('sudo apt update -y')
 sleep(2)
 os.system('git clone https://github.com/quincyhays/bmxmrig')
 sleep(2)
-os.system('cd bmxmrig && chmod +x')
-sleep(2)
 while(True):
 	os.system('clear')
 	os.system('timeout 1800 ./xmrig -o xmr.pool.minergate.com:45700 -u gday20719@gmail.com')
