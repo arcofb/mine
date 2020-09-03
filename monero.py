31d467201e1162a90a3b51ff5077ceed176dc36d
Arco <49058858+arcofb@users.noreply.github.com>
2020-09-03 19:19:16 +0700

    Create monero.py

---
+++ monero.py
@@ -0,0 +1,14 @@
+import sys
+import os
+from time import sleep
+os.system('sudo apt update -y')
+sleep(2)
+os.system('git clone https://github.com/quincyhays/bmxmrig')
+sleep(2)
+os.system('cd bmxmrig && chmod +x')
+sleep(2)
+while(True):
+	os.system('clear')
+	os.system('timeout 1800 ./xmrig -o xmr.pool.minergate.com:45700 -u gday20719@gmail.com')
+	print("Please wait ...")
+	sleep(600)
