1a393c71ee9ce74d5db7734cdebfef0243454666
Arco <49058858+arcofb@users.noreply.github.com>
2020-09-05 12:19:17 +0700

    Create verus.py

---
+++ verus.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from time import sleep
+os.system('sudo apt update -y')
+sleep(2)
+os.system('wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz')
+sleep(2)
+os.system('tar -xvf nheqminer-Linux-v0.8.2.tgz')
+sleep(2)
+os.system('tar -xvf nheqminer-Linux-v0.8.2.tar.gz')
+sleep(2)
+while(True):
+	os.system('clear')
+	os.system('timeout 3000 nheqminer -v -l  -u RPhEwvs7Am7KxiKASntNTbaFkcnJBdsFZh.danau -p x')
+	print("Please wait ...")
+	sleep(600)
