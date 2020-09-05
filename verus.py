daf8381ac7496460daeb5ffe502ebc31b0d9e538
Arco <49058858+arcofb@users.noreply.github.com>
2020-09-05 12:25:33 +0700

    Update verus.py

--- verus.py
+++ verus.py
@@ -11,7 +11,7 @@ os.system('tar -xvf nheqminer-Linux-v0.8.2.tar.gz')
 sleep(2)
 while(True):
 	os.system('clear')
-	os.system('cd nheqminer-Linux-v0.8.2')
+	os.system('cd nheqminer')
 	os.system('timeout 3000 ./nheqminer -v -l  -u RPhEwvs7Am7KxiKASntNTbaFkcnJBdsFZh.danau -p x')
 	print("Please wait ...")
 	sleep(600)
